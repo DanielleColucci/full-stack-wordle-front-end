9c16a046ef53cda43fe173e80457ebaf2789bc84
DanielleColucci <ddcolucci99@gmail.com>
2023-03-13 15:31:10 -0400

    stub up Row component and render in Board component

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,13 @@
+import Row from "../Row/Row"
+
 const Board = (props) => {
+  const guessLimitArr = new Array(props.guessLimit)
   return (
-    <div>board component</div>
+    <div className="board">
+      {guessLimitArr.map((x, idx) => (
+        <Row key={idx} />
+      ))}
+    </div>
   )
 }
 
