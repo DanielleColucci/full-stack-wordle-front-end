d2c57e2e29f11a9241d8cb019a035744f3a09d49
DanielleColucci <ddcolucci99@gmail.com>
2023-03-14 15:18:32 -0400

    extend Row styling to UsedRow and EmptyRow

--- src/components/Row/EmptyRow.jsx
+++ src/components/Row/EmptyRow.jsx
@@ -1,6 +1,8 @@
+import styles from './Row.module.css'
+
 const EmptyRow = () => {
   return (
-    <div className="row">
+    <div className={styles.row}>
       <div></div>
       <div></div>
       <div></div>
--- src/components/Row/UsedRow.jsx
+++ src/components/Row/UsedRow.jsx
@@ -1,7 +1,9 @@
+import styles from './Row.module.css'
+
 const UsedRow = ({ guess }) => {
   const guessArr = guess.split('')
   return (
-    <div className="row">
+    <div className={styles.row}>
       {guessArr.map((letter, idx) => (
         <div key={idx}>{letter}</div>
       ))}
