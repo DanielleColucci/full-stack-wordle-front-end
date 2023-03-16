d18b03d0bd1b2222f6d3b50ecda5c7c485dfddaf
DanielleColucci <ddcolucci99@gmail.com>
2023-03-16 13:52:06 -0400

    define and export checkLoss utility function

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -2,6 +2,12 @@ function checkWinner(guesses, secretWords) {
   return secretWords.every(word => guesses.includes(word))
 }
 
+function checkLoss(guesses, guessLimit) {
+  if (guesses.length >= guessLimit) return true
+  else return false
+}
+
 export {
   checkWinner,
+  checkLoss, 
 }
