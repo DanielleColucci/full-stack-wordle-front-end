7b4e3190a530d10789b6b51396fcdf323d7028dc
DanielleColucci <ddcolucci99@gmail.com>
2023-03-17 11:00:03 -0400

    remove broken code from getColorArray

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -11,21 +11,18 @@ function getColorArray(currentGuess, word) {
   let lowerGuessArr = currentGuess.split('')
   let wordArr = word.split('')
   let colorArr = []
-  lowerGuessArr.forEach(function(char, idx) {
+  lowerGuessArr.forEach((char, idx) => {
     if (char === wordArr[idx]) {
-      document.getElementById(char).classList.add('green')
       colorArr.splice(idx, 0, 'green')
       lowerGuessArr[idx] = ' '
       wordArr[idx] = ' '
     } 
   }) 
-  lowerGuessArr.forEach(function(char, idx) {
+  lowerGuessArr.forEach((char, idx) => {
     if (wordArr.includes(char) && char !== wordArr[idx]) {
-      document.getElementById(char).classList.add('yellow')
       colorArr.splice(idx, 0, 'yellow')
       wordArr[wordArr.indexOf(char)] = ' '
     } else if (char !== ' ') {
-      document.getElementById(char).classList.add('grey')
       colorArr.splice(idx, 0, 'grey')
     }
   })
