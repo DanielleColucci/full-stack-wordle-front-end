74eef6f1316fcba0a5658ee62aa9762bd0243e31
DanielleColucci <ddcolucci99@gmail.com>
2023-03-29 12:09:33 -0400

    attach click handler to all Keyboard keys

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -5,39 +5,39 @@ const Keyboard = (props) => {
     <div className={styles.keyboard}>
       <div className={styles.keyboardRow}>
         <div id='q' onClick={props.onClick} className={styles.key}>Q</div>
-        <div id='w' className={styles.key}>W</div>
-        <div id='e' className={styles.key}>E</div>
-        <div id='r' className={styles.key}>R</div>
-        <div id='t' className={styles.key}>T</div>
-        <div id='y' className={styles.key}>Y</div>
-        <div id='u' className={styles.key}>U</div>
-        <div id='i' className={styles.key}>I</div>
-        <div id='o' className={styles.key}>O</div>
-        <div id='p' className={styles.key}>P</div>
+        <div id='w' onClick={props.onClick} className={styles.key}>W</div>
+        <div id='e' onClick={props.onClick} className={styles.key}>E</div>
+        <div id='r' onClick={props.onClick} className={styles.key}>R</div>
+        <div id='t' onClick={props.onClick} className={styles.key}>T</div>
+        <div id='y' onClick={props.onClick} className={styles.key}>Y</div>
+        <div id='u' onClick={props.onClick} className={styles.key}>U</div>
+        <div id='i' onClick={props.onClick} className={styles.key}>I</div>
+        <div id='o' onClick={props.onClick} className={styles.key}>O</div>
+        <div id='p' onClick={props.onClick} className={styles.key}>P</div>
       </div>
       <div className={styles.keyboardRow}>
-        <div id='a' className={styles.key}>A</div>
-        <div id='s' className={styles.key}>S</div>
-        <div id='d' className={styles.key}>D</div>
-        <div id='f' className={styles.key}>F</div>
-        <div id='g' className={styles.key}>G</div>
-        <div id='h' className={styles.key}>H</div>
-        <div id='j' className={styles.key}>J</div>
-        <div id='k' className={styles.key}>K</div>
-        <div id='l' className={styles.key}>L</div>
-        <div id='enter' className={`${styles.key} ${styles.enterTop}`}></div>
+        <div id='a' onClick={props.onClick} className={styles.key}>A</div>
+        <div id='s' onClick={props.onClick} className={styles.key}>S</div>
+        <div id='d' onClick={props.onClick} className={styles.key}>D</div>
+        <div id='f' onClick={props.onClick} className={styles.key}>F</div>
+        <div id='g' onClick={props.onClick} className={styles.key}>G</div>
+        <div id='h' onClick={props.onClick} className={styles.key}>H</div>
+        <div id='j' onClick={props.onClick} className={styles.key}>J</div>
+        <div id='k' onClick={props.onClick} className={styles.key}>K</div>
+        <div id='l' onClick={props.onClick} className={styles.key}>L</div>
+        <div id='enter' onClick={props.onClick} className={`${styles.key} ${styles.enterTop}`}></div>
       </div>
       <div className={styles.keyboardRow}>
-        <div id='backspace' className={`${styles.key} ${styles.backspace}`}>⌫</div>
-        <div id='z' className={styles.key}>Z</div>
-        <div id='x' className={styles.key}>X</div>
-        <div id='c' className={styles.key}>C</div>
-        <div id='v' className={styles.key}>V</div>
-        <div id='b' className={styles.key}>B</div>
-        <div id='n' className={styles.key}>N</div>
-        <div id='m' className={styles.key}>M</div>
-        <div id='enter' className={`${styles.key} ${styles.enterLeft}`}></div>
-        <div id='enter' className={`${styles.key} ${styles.enterRight}`}>↵</div>
+        <div id='backspace' onClick={props.onClick} className={`${styles.key} ${styles.backspace}`}>⌫</div>
+        <div id='z' onClick={props.onClick} className={styles.key}>Z</div>
+        <div id='x' onClick={props.onClick} className={styles.key}>X</div>
+        <div id='c' onClick={props.onClick} className={styles.key}>C</div>
+        <div id='v' onClick={props.onClick} className={styles.key}>V</div>
+        <div id='b' onClick={props.onClick} className={styles.key}>B</div>
+        <div id='n' onClick={props.onClick} className={styles.key}>N</div>
+        <div id='m' onClick={props.onClick} className={styles.key}>M</div>
+        <div id='enter' onClick={props.onClick} className={`${styles.key} ${styles.enterLeft}`}></div>
+        <div id='enter' onClick={props.onClick} className={`${styles.key} ${styles.enterRight}`}>↵</div>
       </div>
     </div>
   )
