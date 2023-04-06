cd159f130e23b31d8985f043f5d286e3c686fb2c
DanielleColucci <ddcolucci99@gmail.com>
2023-04-06 16:15:41 -0400

    define and export create service function for scores

---
+++ src/services/scoreService.js
@@ -0,0 +1,27 @@
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/scores`
+
+const create = async (mode, numGuesses) => {
+  const scoreData = {
+    mode: mode,
+    value: numGuesses - mode + 1
+  }
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(scoreData)
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export { 
+  create,
+}
