e738403f19e922c3ed48baba21de976d0eb4e445
DanielleColucci <ddcolucci99@gmail.com>
2023-04-07 15:02:48 -0400

    update Results styling

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -2,9 +2,11 @@ import styles from './Results.module.css'
 
 const Results = (props) => {
   return (
-    <>
-      <h1 className={styles.message}>This is a results component</h1>
-    </>
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <h1 className={styles.message}>This is a results component</h1>
+      </div>
+    </div>
   )
 }
 
