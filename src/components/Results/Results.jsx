428f5b388a553f9c9c2a30a17611e79a80e9e818
DanielleColucci <ddcolucci99@gmail.com>
2023-04-07 16:32:48 -0400

    update Results content

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -13,12 +13,15 @@ const Results = ({ wordCount, user, message}) => {
     fetchScores()
   },[user, wordCount])
 
-  // counting number of each win option (count+1, count+2, etc) and losses
+  // counting number of each win option (count+1, count+2, etc) and losses, then calculating percentages
   // array is of format [1, 2, 3, 4, 5, 6, losses]
   const resultsArr = new Array(7).fill(0)
   scores.forEach(s => {
     if (!s.win) resultsArr[6]++
     else resultsArr[s.value - 1]++
+  })
+  resultsArr.forEach((result,idx) => {
+    resultsArr[idx] = Math.floor(result / scores.length * 100)
   })
 
   return (
@@ -28,33 +31,51 @@ const Results = ({ wordCount, user, message}) => {
         <div className={styles.resultsContainer}>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[0] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[0]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[0]}</div>
           </div>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount + 1}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[1] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[2]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[1]}</div>
           </div>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount + 2}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[2] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[2]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[2]}</div>
           </div>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount + 3}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[3] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[3]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[3]}</div>
           </div>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount + 4}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[4] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[4]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[4]}</div>
           </div>
           <div className={styles.resultsRow}>
             <div className={styles.label}>{wordCount + 5}</div>
-            <div className={styles.bar}></div>
-            <div className={styles.result}>{Math.round(resultsArr[5] / scores.length * 100)}</div>
+            <div 
+              className={styles.bar}
+              style={{width: `${resultsArr[5]}%`}}
+            ></div>
+            <div className={styles.result}>{resultsArr[5]}</div>
           </div>
         </div>
       </div>
