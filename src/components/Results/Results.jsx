ea2082ba1ac31fcf3a864c1700f5fb57d0f6171b
DanielleColucci <ddcolucci99@gmail.com>
2023-04-07 15:59:56 -0400

    fetch scores for mode and store in Results state

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,18 @@
 import styles from './Results.module.css'
+import { useState, useEffect } from 'react'
+import * as scoreService from '../../services/scoreService'
 
 const Results = (props) => {
+  const [scores, setScores] = useState([])
+
+  useEffect(() => {
+    const fetchScores = async () => {
+      const data = await scoreService.modeIndex(props.wordCount, props.user)
+      setScores(data)
+    }
+    fetchScores()
+  },[props.user, props.wordCount])
+
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
