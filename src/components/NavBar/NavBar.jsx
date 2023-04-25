23d4a5ca10692ee7686705a79d2b424617d52b16
DanielleColucci <ddcolucci99@gmail.com>
2023-04-25 16:54:56 -0400

    add play link in nav bar and update styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,14 +6,24 @@ const NavBar = ({ user, handleLogout }) => {
     <nav className={styles.nav}>
       {user ?
         <div className={styles.container}>
-          <Link to='/profiles'>Profiles</Link>
-          <Link to='' onClick={handleLogout}>LOG OUT</Link>
-          <Link to='/change-password'>Change Password</Link>
+          <div className={styles.navLeft}>
+            <Link to='/play'>PLAY</Link>
+            <Link to='/profiles'>PROFILES</Link>
+          </div>
+          <div className={styles.navRight}>
+            <Link to='' onClick={handleLogout}>LOG OUT</Link>
+            <Link to='/change-password'>CHANGE PASSWORD</Link>
+          </div>
         </div>
       :
-        <div className={styles.container}>
-          <Link to='/login'>LOG IN</Link>
-          <Link to='/signup'>SIGN UP</Link>
+      <div className={styles.container}>
+          <div className={styles.navLeft}>
+            <Link to='/play'>PLAY</Link>
+          </div>
+          <div className={styles.navRight}>
+            <Link to='/login'>LOG IN</Link>
+            <Link to='/signup'>SIGN UP</Link>
+          </div>
         </div>
       }
     </nav>
